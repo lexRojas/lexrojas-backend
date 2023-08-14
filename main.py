cf9b7f225325a3081aa40db0917b13551b989362
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-08-14 11:01:56 -0600

    variable PORT sin definir

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("app.api:app", host="0.0.0.0", reload=True)
+    # uvicorn.run("app.api:app", host="0.0.0.0", port=5000, reload=True)
