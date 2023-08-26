65a6a7f4dad90b59651d230aed46d0242fe6cd35
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-08-26 07:53:44 -0600

    version_local

--- routes/notas.py
+++ routes/notas.py
@@ -4,13 +4,13 @@ from config.db  import conn
 notas = APIRouter();
 
 @notas.get("/notas")
-async def getNotas(id = "-1"):
+async def getNotas(id = "-1", idMateria="1"):
       with conn.cursor() as cursor:
         # Read a single record
         if (id=="-1"):
           sql = "select * from notas" 
         else:
-          sql = "SELECT * FROM  notas n inner join estudiante e on e.id = n.id where e.id ="+ str(id)
+          sql = "SELECT * FROM  notas n inner join estudiante e on e.id = n.id where e.id ="+ str(id) + " and n.idMateria = " + str(idMateria)
         cursor.execute(sql)
         result = cursor.fetchall()
         return(result)
