d6ebaf425f150a29575bb940f1e3705ff9eaa89d
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-10-12 19:12:48 -0600

    changed flujo set y get

--- routes/notas_app/flujo.py
+++ routes/notas_app/flujo.py
@@ -1,12 +1,27 @@
 from fastapi import APIRouter
 from config.db  import conn
 
-flujo = APIRouter();
+flujo = APIRouter()
 
-@flujo.post("/flujo",)
+@flujo.post("/flujo")
 async def setFlujo(valor:int):
       with conn.cursor() as cursor:
         # Read a single record
         if ( valor > 0):
-          sql = "insert into flujo values ({valor})" 
+          sql = f"INSERT INTO flujo (valor) VALUES ({valor});"
+          print (sql) 
+          i = cursor.execute(sql)
+          print (i)
+          conn.commit
+      return {"message": "Employee added successfully"}
+
+
+
+@flujo.get("/flujo")
+async def getFlujo():
+      with conn.cursor() as cursor:
+        # Read a single record
+        sql = f"SELECT * FROM  flujo;"
         cursor.execute(sql)
+        result = cursor.fetchall
+        return result 
