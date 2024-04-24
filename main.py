a09d5c673e5f80dfc9513dd1f23eeff0b99ff4d3
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-04-24 13:51:59 -0600

    port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="localhost", port=8000, reload=True)
-    # uvicorn.run("app.api:app", host="0.0.0.0", port=5000, reload=True)
+if __name__== "__main__":
+    uvicorn.run("app:app", reload=True)
+
